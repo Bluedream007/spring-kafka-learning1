dfa7e55962921590591a55772670e3c7496cf3e6
charlotte liu <liu.charlotte@gmail.com>
2022-07-19 12:13:01 +0800

    Add new project - Kafka Avro

---
+++ springcloud-kafka-avro1/src/test/java/com/bluedream/sample/springcloudkafkaavro1/SpringcloudKafkaAvro1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluedream.sample.springcloudkafkaavro1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudKafkaAvro1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
